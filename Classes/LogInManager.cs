49d78fcdf5d27a1d4b6a671aba46dfd13c7d8a0c
Krisuuuuuu <krisu1997@onet.pl>
2019-12-18 21:27:58 +0100

    Fixed bugs related to login method

--- Classes/LogInManager.cs
+++ Classes/LogInManager.cs
@@ -16,11 +16,11 @@ namespace Bank_App.Classes
         public static bool LogIn(string login, string password, out bool isAdmin)
         {
             DataTable datatable = DataBaseManager.Get("SELECT * FROM UserTable WHERE Login = " + "'" + login + "' AND PASSWORD = " + "'" + password + "'");
-            int userId= Convert.ToInt32(datatable.Rows[0]["Id"]);
             try
             {
                 if (datatable.Rows.Count == 1)
                 {
+                    int userId = Convert.ToInt32(datatable.Rows[0]["Id"]);
                     DataTable data = DataBaseManager.Get("SELECT * FROM AccountTable WHERE UserId = " + "'" + userId + "'");
                     WhoIsCurrentLoged = data.Rows[0]["AccountNumber"].ToString();
                     if(datatable.Rows[0]["isAdmin"].ToString() == "True")
