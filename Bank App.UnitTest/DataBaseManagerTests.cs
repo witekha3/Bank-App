931b47af1f460936291a13fc215700a4948aa1ea
Witold <46688375+witekha3@users.noreply.github.com>
2020-01-04 11:13:11 +0100

    Adding unit tests project

---
+++ Bank App.UnitTest/DataBaseManagerTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bank_App.Classes;
+using Bank_App.Forms;
+
+namespace Bank_App.UnitTest
+{
+    [TestClass]
+    public class DataBaseManagerTests
+    {
+        [TestMethod]
+        public void Get_SendCorrectSqlQuery_ReturnsDataTable()
+        {
+            DataBaseManager databaseManager = new DataBaseManager();
+            var result = databaseManager.Get("SELECT * FROM UserTable");
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void Get_SendIncorrectSqlQuery_ReturnsZero()
+        {
+            DataBaseManager databaseManager = new DataBaseManager();
+            var result = databaseManager.Get("SELECT * FROM dawdwaf");
+            Assert.AreEqual(0, result.Rows.Count);
+
+        }
+    }
+}
---
+++ Bank App.UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Bank App.UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Bank App.UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("eaf203a8-b9b2-4c97-b573-fa6457a52034")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
